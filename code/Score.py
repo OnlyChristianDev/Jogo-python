e35d690ba2d58fbf1262ef72bd2597acad5a7012
Christian Gabriel <christiandeveloper123@gmail.com>
2026-05-11 21:26:16 -0300

    Adicionando condição de vitória

--- code/Score.py
+++ code/Score.py
@@ -3,11 +3,18 @@ import pygame
 class Score:
     def __init__(self):
         self.value = 0
+        self.won = False
         self.font = pygame.font.SysFont("monoespace", 30, bold=True)
 
     def update(self, dt):
-        self.value += dt * 10 
+        if self.won:
+            return
+
+        self.value += dt * 10
+        if self.value >= 1000:
+            self.value = 1000
+            self.won = True
 
     def draw(self, screen):
-        text = self.font.render(f"Score: {int(self.value)}", True, (255, 255, 255))
+        text = self.font.render(f"Score: {int(self.value)}/1000", True, (255, 255, 255))
         screen.blit(text, (10, 40)) 
